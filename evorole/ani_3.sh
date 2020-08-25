be5a5e01260dc53572f8c7f66bf88fdd18f31164
Xiaoyan2 <xiaoyan.long2@gmail.com>
2020-08-25 14:53:21 +0200

    more anisogamy rep

---
+++ evorole/ani_3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#!!! adjust the time quota !!!
+
+#SBATCH --time=23:30:00
+#SBATCH --partition=gelifes
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --mem=1000
+#SBATCH --cpus-per-task=16
+
+module load GCC --latest
+
+datadir="/data/$(whoami)/ani_3"
+./bin/evorole --force param=param.json rep=20 outdir=$datadir
