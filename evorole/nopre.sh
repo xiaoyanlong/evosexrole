5bf2e8793c240744041b058c1be66cb17e8fcb54
richelbilderbeek <richel@richelbilderbeek.nl>
2021-06-27 15:47:01 +0200

    pre

--- evorole/nopre.sh
+++ evorole/nopre.sh
@@ -2,11 +2,11 @@
 
 #!!! adjust the time quota !!!
 
-#SBATCH --time=02:15:00
+#SBATCH --time=00:15:00
 #SBATCH --partition=gelifes
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --mem=10GB
+#SBATCH --mem=20GB
 #SBATCH --cpus-per-task=16
 
 module load GCC --latest
