038b603d1bd50537375145936eaf1a6450170039
richelbilderbeek <richel@richelbilderbeek.nl>
2021-06-27 15:00:27 +0200

    pre

---
+++ evorole/nopre.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#!!! adjust the time quota !!!
+
+#SBATCH --time=02:15:00
+#SBATCH --partition=gelifes
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --mem=10GB
+#SBATCH --cpus-per-task=16
+
+module load GCC --latest
+
+datadir="/data/$(whoami)"
+./bin/evorole --force param=nopre.json rep=100 outdir=$datadir/2021-06-23-nopre
