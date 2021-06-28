689cc64b05c593d026d8ec90f30000d2e07c480a
richelbilderbeek <richel@richelbilderbeek.nl>
2021-06-28 22:00:26 +0200

    pre

---
+++ evorole/pre_male_0.001.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#!!! adjust the time quota !!!
+
+#SBATCH --time=05:15:00
+#SBATCH --partition=gelifes
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --mem=15GB
+#SBATCH --cpus-per-task=16
+
+module load GCC --latest
+
+datadir="/data/$(whoami)"
+./bin/evorole --force param=pre_male_0.001.json rep=50 outdir=$datadir/2021-06-27-pre_male_0.001
