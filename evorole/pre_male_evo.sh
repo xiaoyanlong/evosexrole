f32dc87f545ad6e912ec47664bf3f0d29aa9f0a1
richelbilderbeek <richel@richelbilderbeek.nl>
2021-07-02 11:25:53 +0200

    mating-day

--- evorole/pre_male_evo.sh
+++ evorole/pre_male_evo.sh
@@ -2,14 +2,14 @@
 
 #!!! adjust the time quota !!!
 
-#SBATCH --time=01:15:00
+#SBATCH --time=02:15:00
 #SBATCH --partition=gelifes
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --mem=15GB
+#SBATCH --mem=25GB
 #SBATCH --cpus-per-task=16
 
 module load GCC --latest
 
 datadir="/data/$(whoami)"
-./bin/evorole --force param=pre_female_0.json rep=50 outdir=$datadir/2021-07-02-pre_female_0
+./bin/evorole --force param=pre_male_evo.json rep=10 outdir=$datadir/2021-07-02-pre_male_evo
